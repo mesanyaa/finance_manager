65addb9249440f40c0733eb310f91f0bf8fa8377
mesanyaa <iamsashayakovchuk@gmail.com>
2024-11-17 14:40:57 +0300

    Свёрстана страница 404

--- src/pages/404/ui/404.tsx
+++ src/pages/404/ui/404.tsx
@@ -1,5 +1,21 @@
+import { Link } from 'react-router-dom';
+
+import styles from './styles.module.css';
+
 const Page404 = () => {
-    return <>404</>;
+    return (
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <div className={styles.errorCode}>404</div>
+                <div className={styles.errorText}>
+                    Похоже страница с таким url не найдена
+                </div>
+                <Link to="/" className={styles.linkToHomePage}>
+                    нажмите, чтобы перейти на главную
+                </Link>
+            </div>
+        </div>
+    );
 };
 
 export { Page404 };
