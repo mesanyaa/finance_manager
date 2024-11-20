7328cae016a9949fa01520579937c65f6c6c20e8
mesanyaa <iamsashayakovchuk@gmail.com>
2024-11-20 23:07:09 +0300

    Responsive design addded

--- src/shared/categoriesItem/ui/CategoriesItem.tsx
+++ src/shared/categoriesItem/ui/CategoriesItem.tsx
@@ -1,24 +1,15 @@
 import { FC } from 'react';
 
-import trashBin from '../../../assets/trash-bin.svg';
-
 import styles from './styles.module.css';
 
 interface CategoriesItemProps {
     categoryName: string;
 }
 
-const CategoriesItem: FC<CategoriesItemProps> = ({
-    categoryName,
-}) => {
+const CategoriesItem: FC<CategoriesItemProps> = ({ categoryName }) => {
     return (
         <div className={styles.container}>
             <div className={styles.categoryName}>{categoryName}</div>
-            <img
-                src={trashBin}
-                alt="trashBin"
-                className={styles.trashBin}
-            />
         </div>
     );
 };
